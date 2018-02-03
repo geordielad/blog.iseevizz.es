36d55938b7072dfaaa0aaf125b2c417cd25fbda6
geordielad <rcottiss@tableau.com>
2018-02-03 16:49:52 -0500

    Remove graph plugin it causes UglifyJS error

--- gatsby-config.js
+++ gatsby-config.js
@@ -99,14 +99,6 @@ module.exports = {
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-graph',
-            options: {
-            // this is the language in your code-block that triggers mermaid parsing
-              language: 'mermaid', // default
-              theme: 'default' // could also be dark, forest, or neutral
-            }
-          },       
-          {
             resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 960
