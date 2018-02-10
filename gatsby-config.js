e5072a17204df66c53057d3e8aa2ef10ce076f58
geordielad <rcottiss@tableau.com>
2018-02-09 22:34:15 -0500

    Added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,24 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/favicon.png",
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/pages`,
